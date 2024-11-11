9cf4cf2e74c1e0941a58cf514f32f1a1f44e6bb5
GOA-gp <gimpo.s230361@ggh.goe.go.kr>
2024-11-11 11:34:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,19 @@
 import streamlit as st
 
 # 1. 기계학습 모델 파일 로드
-
+import joblib
+model = joblib.load('logistic_regression_model.pkl') 
 
 # 2. 모델 설명
- 
+ st.title('하루 게시물 수에 따른 팔로워수 증가 예측 모델')
 
 # 3. 데이터시각화
-
+with col2:
+      st.subheader('')
+      st.image('' )   # 이미지 불러오기
+with col3:
+      st.subheader('')
+      st.image('')    # 이미지 불러오기
 
 # 4. 모델 활용
+
